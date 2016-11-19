786b5d1902de85b4843e702f016f262de0b87e5a
forumber <kidobeko@gmail.com>
2016-11-19 21:08:17 +0300

    Tidy up

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ add_lunch_combo cm_turkcellt50-userdebug
 cd hardware/qcom/audio-caf/msm8974
 if grep -q "acdb_send_audio_cal_t)dlsym(my_data->acdb_handle" hal/msm8974/platform.c
 then
-    git am ../../../../device/zte/turkcellt50/audio/0001-fix-audio.patch || git am --abort
+    git am ../../../../device/zte/turkcellt50/patches/0001-fix-audio.patch || git am --abort
     echo 'Audio fix for T50 is now applied';
 else
     echo 'Audio fix for T50 is already applied';
@@ -13,7 +13,7 @@ croot
 cd build
 if grep -q "block " core/Makefile
 then
-    git am ../device/zte/turkcellt50/Disable-block-based-installation.patch || git am --abort
+    git am ../device/zte/turkcellt50/patches/Disable-block-based-installation.patch || git am --abort
     echo 'Disabled block based flashing';
 else
     echo 'Block based flashing is already disabled';
